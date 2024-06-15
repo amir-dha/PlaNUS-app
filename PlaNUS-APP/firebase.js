6abb4b426bb5a137468c16967e17c5914e96cca7
amir-dha <ravichandranamirdha25@gmail.com>
2024-06-15 12:36:04 +0800

    firebase set-up

---
+++ PlaNUS-APP/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh6f-ePbhkTzfMtv7O3c5AQ39HL22nQrI",
+  authDomain: "planus-app-65d36.firebaseapp.com",
+  projectId: "planus-app-65d36",
+  storageBucket: "planus-app-65d36.appspot.com",
+  messagingSenderId: "988833197342",
+  appId: "1:988833197342:web:e1d1b0275a5e68943dad0f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export { auth, db };
