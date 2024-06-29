6f48e6449beb649b889c4bc6d29e9f76e6ec0685
amir-dha <ravichandranamirdha25@gmail.com>
2024-06-30 04:10:33 +0800

    email verification, resend email verification and forget password added. set up database such that without verification for a certain period of time, the account will be deleted.

--- PlaNUS-APP/Screens/Authentication Screens/LoginScreen.js
+++ PlaNUS-APP/Screens/Authentication Screens/LoginScreen.js
@@ -1,7 +1,178 @@
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
+// import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
+// import React, { useEffect, useState } from 'react';
+// import { auth } from '../../firebase';
+// import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+// import { useNavigation } from '@react-navigation/core';
+// import { Ionicons } from '@expo/vector-icons';
+
+// const logo = require('../../assets/logo.png'); 
+
+// const LoginScreen = () => {
+//     const [email, setEmail] = useState(''); 
+//     const [password, setPassword] = useState('');
+
+//     const navigation = useNavigation();
+
+//     //Navigate to HomeScreen once Login button has been pressed
+//     useEffect(() => {
+//         const unsubscribe = onAuthStateChanged(auth, user => {
+//             if (user) {
+//                navigation.navigate("Home");
+//             }
+//         });
+//         return unsubscribe; 
+//     }, []);
+
+//     const isValidEmail = email => {
+//         const regex = /^e\d{7}@u\.nus\.edu$/;
+//         const isValid = regex.test(email);
+//         console.log(`Email validation for ${email}: ${isValid}`);
+//         return isValid;
+//       };
+
+//     const handleLogin = () => {
+//         if (!isValidEmail(email)) {
+//             alert('Invalid email. Not an NUS Student email.');
+//             return;
+//         }
+//         signInWithEmailAndPassword(auth, email, password)
+//         .then(userCredentials => {
+//          const user = userCredentials.user; 
+//         })
+//         .catch(error => {
+//             console.log(`Error code: ${error.code}`);
+//             console.error('Login error:', error); // Added error logging
+//             switch (error.code) {
+//                 case 'auth/user-not-found':
+//                     alert('The account does not exist.');
+//                     break;
+//                 case 'auth/invalid-credential':
+//                     alert('Invalid email or password. Try again.');
+//                     break;
+//                 // case 'auth/wrong-password': 
+//                 //     alert('Incorrect password. Please try again.');
+//                     // break;
+//                 default:
+//                     alert(error.message);
+//             }
+//         });
+//     };
+
+//   return (
+//     <KeyboardAvoidingView
+//         style={styles.container}
+//         behavior='padding'
+//     >
+//         <TouchableOpacity 
+//             style={styles.backButton}
+//             onPress={() => navigation.goBack()}
+//         >
+//             <Ionicons name='chevron-back' size = {24} color='white' />
+//         </TouchableOpacity>
+//         <Image 
+//             source={logo}
+//             style={styles.image}
+//         />
+
+//         <Text style={styles.headerText}>Welcome Back!</Text>  
+
+//         <View style={styles.inputContainer}>
+//             <TextInput
+//                 placeholder='Email'
+//                 value={email}
+//                 onChangeText={text => setEmail(text)}
+//                 style={styles.input}
+//                 autoCapitalize='none'
+//             />
+//             <TextInput
+//                 placeholder='Password'
+//                 value={password}
+//                 onChangeText={text => setPassword(text)}
+//                 style={styles.input}
+//                 autoCapitalize='none'
+//                 secureTextEntry
+//             />
+//         </View>
+
+//         <View style={styles.buttonContainer}>
+//             <TouchableOpacity
+//                 onPress={handleLogin}
+//                 style={styles.button}
+//             >
+//                 <Text style={styles.buttonInput}>Log In</Text>
+//             </TouchableOpacity>
+//         </View>
+
+//     </KeyboardAvoidingView>
+//   );
+// };
+
+// export default LoginScreen
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1, 
+//         justifyContent: 'center', 
+//         alignItems:'center',
+//         padding: 40,
+//         backgroundColor:'#003882',
+//     },
+//     image: {
+//         width:150, 
+//         height:100,
+//         marginBottom: 30,
+//         marginLeft:20,
+//     },
+//     headerText: {
+//         color:'white', 
+//         fontSize: 40,
+//         fontFamily:'Ubuntu-Regular',
+//         textAlign:'center',
+//         marginBottom: 20,
+//     },
+//     inputContainer: {
+//         width:'80%',
+//     },
+//     input: {
+//         backgroundColor:'white',
+//         paddingVertical:10,
+//         paddingHorizontal:15,
+//         borderRadius:25,
+//         marginBottom:15,
+//         alignItems:'center',
+//         fontFamily:'Ubuntu-Regular',
+//     }, 
+//     buttonContainer:{
+//         width:'60%',
+//         justifyContent:'center',
+//         alignItems:'center',
+//         marginTop: 30,
+//     },
+//     button: {
+//         backgroundColor:'white',
+//         width:'100%',
+//         padding:12,
+//         alignItems:'center',
+//         borderRadius:25,
+//         borderWidth:2,
+//         borderColor:'#e7e7e7',
+//     },
+//     buttonInput: {
+//         color:'#003882',
+//         fontSize:16,
+//         fontFamily:'Ubuntu-Bold',
+//     },
+//     backButton: {
+//         position:'absolute',
+//         top: 50,
+//         left: 20,
+//     },
+//     });
+
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image, Alert } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { auth } from '../../firebase';
-import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import { onAuthStateChanged, signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 import { useNavigation } from '@react-navigation/core';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -13,11 +184,11 @@ const LoginScreen = () => {
 
     const navigation = useNavigation();
 
-    //Navigate to HomeScreen once Login button has been pressed
+    // Navigate to HomeScreen once Login button has been pressed
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, user => {
             if (user) {
-               navigation.navigate("Home");
+                navigation.navigate("Home");
             }
         });
         return unsubscribe; 
@@ -28,7 +199,7 @@ const LoginScreen = () => {
         const isValid = regex.test(email);
         console.log(`Email validation for ${email}: ${isValid}`);
         return isValid;
-      };
+    };
 
     const handleLogin = () => {
         if (!isValidEmail(email)) {
@@ -37,7 +208,7 @@ const LoginScreen = () => {
         }
         signInWithEmailAndPassword(auth, email, password)
         .then(userCredentials => {
-         const user = userCredentials.user; 
+            const user = userCredentials.user; 
         })
         .catch(error => {
             console.log(`Error code: ${error.code}`);
@@ -49,62 +220,86 @@ const LoginScreen = () => {
                 case 'auth/invalid-credential':
                     alert('Invalid email or password. Try again.');
                     break;
-                // case 'auth/wrong-password': 
-                //     alert('Incorrect password. Please try again.');
-                    // break;
                 default:
                     alert(error.message);
             }
         });
     };
 
-  return (
-    <KeyboardAvoidingView
-        style={styles.container}
-        behavior='padding'
-    >
-        <TouchableOpacity 
-            style={styles.backButton}
-            onPress={() => navigation.goBack()}
+    const handleForgetPassword = () => {
+        if (!isValidEmail(email)) {
+            alert('Invalid email. Not an NUS Student email.');
+            return;
+        }
+        sendPasswordResetEmail(auth, email)
+        .then(() => {
+            alert('Password reset email sent. Please check your inbox.');
+        })
+        .catch(error => {
+            console.error('Error sending password reset email:', error);
+            switch (error.code) {
+                case 'auth/user-not-found':
+                    alert('The account does not exist.');
+                    break;
+                default:
+                    alert('Failed to send password reset email. Please try again.');
+            }
+        });
+    };
+
+    return (
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior='padding'
         >
-            <Ionicons name='chevron-back' size = {24} color='white' />
-        </TouchableOpacity>
-        <Image 
-            source={logo}
-            style={styles.image}
-        />
-
-        <Text style={styles.headerText}>Welcome Back!</Text>  
-
-        <View style={styles.inputContainer}>
-            <TextInput
-                placeholder='Email'
-                value={email}
-                onChangeText={text => setEmail(text)}
-                style={styles.input}
-                autoCapitalize='none'
-            />
-            <TextInput
-                placeholder='Password'
-                value={password}
-                onChangeText={text => setPassword(text)}
-                style={styles.input}
-                autoCapitalize='none'
-                secureTextEntry
-            />
-        </View>
-
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity
-                onPress={handleLogin}
-                style={styles.button}
+            <TouchableOpacity 
+                style={styles.backButton}
+                onPress={() => navigation.goBack()}
             >
-                <Text style={styles.buttonInput}>Log In</Text>
+                <Ionicons name='chevron-back' size = {24} color='white' />
             </TouchableOpacity>
-        </View>
+            <Image 
+                source={logo}
+                style={styles.image}
+            />
 
-    </KeyboardAvoidingView>
-  );
+            <Text style={styles.headerText}>Welcome Back!</Text>  
+
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder='Email'
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    style={styles.input}
+                    autoCapitalize='none'
+                />
+                <TextInput
+                    placeholder='Password'
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    style={styles.input}
+                    autoCapitalize='none'
+                    secureTextEntry
+                />
+            </View>
+
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    onPress={handleLogin}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonInput}>Log In</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={handleForgetPassword}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonInput}>Forget Password</Text>
+                </TouchableOpacity>
+            </View>
+
+        </KeyboardAvoidingView>
+    );
 };
 
 export default LoginScreen
@@ -156,6 +351,7 @@ const styles = StyleSheet.create({
         borderRadius:25,
         borderWidth:2,
         borderColor:'#e7e7e7',
+        marginTop: 10,
     },
     buttonInput: {
         color:'#003882',
@@ -167,4 +363,4 @@ const styles = StyleSheet.create({
         top: 50,
         left: 20,
     },
-    });
+});
